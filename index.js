338d008360b2858c145dea49ab4d52155e97c4a5
Colin <owens106@purde.edu>
2018-05-17 14:53:47 -0400

    updated html and js

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = {
+  init: function(formSelector) {
+    this.max = 0
+
+    document
+      .querySelector(formSelector)
+      .addEventListener('submit', ev => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+  },
+
+  handleSubmit: function(ev) {
+    const f = ev.target
+    const flick = {
+      id: ++this.max,
+      name: f.flickName.value,
+    }
+    console.log(flick)
+    f.reset()
+  },
+}
+
+app.init('#flickForm')
